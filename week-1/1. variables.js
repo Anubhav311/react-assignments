6333410cbd42abc95e61eea585ce3a794dfb0ced
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-09-12 11:24:37 +0530

    Update 1. variables.js

--- week-1/1. variables.js
+++ week-1/1. variables.js
@@ -6,16 +6,16 @@ Create variables to store and log information:
 
 Explore data types and type conversion:
  - Create variables of different data types (string, number, boolean).
- - Use typeof to check the data type of variables.
- - Convert a string to a number and vice versa.
+ - Use typeof to check the data type of variables and console.log it.
+ - Convert a string to a number and vice versa and return it from a function.
 
  Arrays:
 Work with arrays:
- - Create an array of your favorite fruits and display them.
- - Write a program to find the largest number in an array.
+ - Create an array of your favorite fruits and log them.
+ - Write a program to find the largest number in an array and log it.
 
  Objects:
 Explore objects:
  - Create an object representing a person with properties like name, age, and country.
- - Write a function that takes a person object as an argument and displays their information.
+ - Write a function that takes a person object as an argument and logs their information.
 */
