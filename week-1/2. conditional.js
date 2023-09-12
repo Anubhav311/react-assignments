5356b19fe19d5067db3d62ad240177a1c7761e1d
Anubhav Bhambri <anubhavbhambri1@gmail.com>
2023-09-12 11:24:54 +0530

    Update 2. conditional.js

--- week-1/2. conditional.js
+++ week-1/2. conditional.js
@@ -2,4 +2,5 @@
 Introduce conditional statements:
  - Write a program that checks if a given number is even or odd.
  - Create a basic calculator that can add, subtract, multiply, or divide two numbers based on user input.
+ - result should be logged in the terminal
  */
