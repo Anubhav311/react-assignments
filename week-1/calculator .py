74c4c1962fd5bf75633881944b01bf797d55d045
Lavesh01125 <laveshpatil01125@gmail.com>
2023-09-16 18:31:17 +0530

    calcultor

---
+++ week-1/calculator .py
@@ -0,0 +1,52 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+def power(x, y):
+    return x ** y
+
+def calculator():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power")
+    print("6. Exit")
+
+    while True:
+        choice = input("Enter choice (1/2/3/4/5/6): ")
+
+        if choice == '6':
+            print("Calculator exiting. Goodbye!")
+            break
+
+        if choice in ('1', '2', '3', '4', '5'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print("Result:", add(num1, num2))
+            elif choice == '2':
+                print("Result:", subtract(num1, num2))
+            elif choice == '3':
+                print("Result:", multiply(num1, num2))
+            elif choice == '4':
+                print("Result:", divide(num1, num2))
+            elif choice == '5':
+                print("Result:", power(num1, num2))
+        else:
+            print("Invalid input. Please try again.")
+
+if __name__ == "__main__":
+    calculator()
